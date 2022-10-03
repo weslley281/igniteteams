d86dfa6564009c2c34755ab9d3f4e13836c50f18
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-03 06:16:02 -0400

    Now I'm using the babel routes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Goups } from './src/screens/Groups';
+import { Goups } from '@screens/Groups';
 
 export default function App() {
   return <Goups />;
