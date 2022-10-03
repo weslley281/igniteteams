c20bcf906959ce7c35e6c4a3eb2c955976392753
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-03 10:31:15 -0400

    creating styles components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export function Goups() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Title>Groops</Title>
+    </Container>
+  );
 }
