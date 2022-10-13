1cfbd903f7ced4f1d2ff0bf199384dfdf96f1021
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-13 12:29:33 -0400

    a

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Goups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import theme from 'src/theme';
 
 export default function App() {
-  return <Goups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Goups />
+    </ThemeProvider>
+  );
 }
