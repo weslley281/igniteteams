d8a724cd9bf898d1dbd539ba2bbe314a11230dcb
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-19 06:21:19 -0400

    a

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from 'react-native';
 import { Goups } from '@screens/Groups';
 import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Goups />
+      {fontsLoaded ? <Goups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
