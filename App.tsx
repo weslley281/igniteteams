218f943d5591f58efd1dbfee71b198dc143ae4a1
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-26 10:40:49 -0400

    using active indicator

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { ActivityIndicator } from 'react-native';
 import { Goups } from '@screens/Groups';
 import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
@@ -7,13 +6,14 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Goups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Goups /> : <Loading />}
     </ThemeProvider>
   );
 }
