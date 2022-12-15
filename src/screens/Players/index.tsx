a6726fbb0b4de1bb9d6888283b11eddb0a72c41e
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-12-15 17:54:28 -0400

    a

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,11 +3,14 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { Input } from '@components/Input';
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container, Form } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+
   return (
     <Container>
       <Header showBackButton />
@@ -23,7 +26,19 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="Time A" isActive />
+      <FlatList
+        data={['Time A', 'Time B']}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Filter
+            key={item}
+            title="Time A"
+            isActive={item === team}
+            onPress={() => setTeam(item)}
+          />
+        )}
+        horizontal
+      />
     </Container>
   );
 }
