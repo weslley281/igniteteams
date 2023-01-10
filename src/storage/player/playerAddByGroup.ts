dfb3e8e73bdb35f9511d07a3e34a9dd11e79d844
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-01-10 15:41:08 -0400

    a

--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -1,5 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { PLAYER_COLLECTION } from '@storage/storageConfig';
+import { AppError } from '@utils/AppError';
+import { playersGetByGroup } from './playerGetByGroup';
 import { PlayerStorageDTO } from './PlayerstorageDTO';
 
 export async function playerAddByGroup(
@@ -7,6 +9,17 @@ export async function playerAddByGroup(
   group: string
 ) {
   try {
+    const storagePlayers = await playersGetByGroup(group);
+    const playerAlreadyExists = storagePlayers.filter(
+      (player) => player.name === newPlayer.name
+    );
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError('Essa pessoa já está adcionada em um time');
+    }
+
+    const storage = JSON.stringify([...storagePlayers, newPlayer]);
+
     await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, '');
   } catch (error) {
     throw error;
